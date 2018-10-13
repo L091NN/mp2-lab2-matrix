135880e72053c3045b47b286bf68cb9353d8da16
Михаил Лугин <329898@mail.ru>
2018-10-13 21:07:02 +0300

    2.Всё готово!

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,11 @@
 #include <gtest.h>
+#include<locale>
 
 int main(int argc, char **argv)
 {
+	setlocale(LC_ALL, "ru");
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  RUN_ALL_TESTS();
+  system("pause");
+  return 0;
 }
